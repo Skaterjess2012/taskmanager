01deb3be9b93da91f83b8f8efedf05a5bd63870f
Jesse <skaterjess2012@gmail.com>
2018-04-12 16:28:16 -0600

    added priority colors to listitems

--- script.js
+++ script.js
@@ -1,36 +1,5 @@
-/*$('.taskinput').on('keyup', function(e){
-	let keypressed = e.keyCode;
-	let inputText = $(this);
-	switch(keypressed) {
-		case 13:
-			if (inputText.val() === "") {
-				break;
-			}
-			addTask(inputText.val());
-			inputText.val('');
-			break;
-	}
-});
-$('.fa-calendar-alt').on('click', function(){
-	$('.priority').slideUp();
-	$('.calendar').slideToggle();
-});
-$('.fa-exclamation').on('click', function(){
-	$('.calendar').slideUp();
-	$('.priority').slideToggle();
-});
-
-function addTask(text) {
-	let list = $('.list');
-	let listItem = $('<li class="listItem"><p>' + text + '</p></input></li>');
-	listItem.appendTo(list);
-	list.children().last().slideDown('fast');
-	list.children().last().css('display', 'flex');
-}
-
-function deleteTask(){}
-*/
 let priority = 'p4'; //sets the defualt priority to p4
+let pColor = {p1:'red', p2:'yellow', p3:'lightblue', p4:'grey', comp:'',};
 
 $('.priority-button').on('click', function(){
 	$('.priority').slideToggle();
@@ -44,10 +13,10 @@ $('.fa-calendar-alt').on('click', function(){
 
 $('.priority-item').on('click', function(){
 	let color;
-	if ($(this).hasClass('priority-level-1')){sP('p1', 'red');}
-	if ($(this).hasClass('priority-level-2')){sP('p2', 'yellow');}
-	if ($(this).hasClass('priority-level-3')){sP('p3', 'lightblue');}
-	if ($(this).hasClass('priority-level-4')){sP('p4', 'grey');}
+	if ($(this).hasClass('priority-level-1')){sP('p1', pColor.p1);}
+	if ($(this).hasClass('priority-level-2')){sP('p2', pColor.p2);}
+	if ($(this).hasClass('priority-level-3')){sP('p3', pColor.p3);}
+	if ($(this).hasClass('priority-level-4')){sP('p4', pColor.p4);}
 	function sP(level, colorType){ //set priotrity
 		priority = level;
 		color = colorType;
@@ -91,7 +60,7 @@ $(document).on('click', '.check', function(){
 	$(this).parent().slideUp('fast', function(){
 		this.remove();
 	});
-	let item = new task(priority, itemText);
+	let item = new task('comp', itemText);
 });
 
 $('.taskinput').on('keyup', function(e){
@@ -114,22 +83,24 @@ class task {
 		let listItem = $('<li class="listItem"><div class="check"><i class="fas fa-check"></i></div><div class="text-container"><p>' + this.text + '</p></div><div class="delete"><i class="fas fa-minus-square"></i></div></input></li>');
 		switch(p) {
 			case 'p1':
-				pi(prio.p1);
+				pi(prio.p1, pColor.p1);
 				break;
 			case 'p2':
-				pi(prio.p2);
+				pi(prio.p2, pColor.p2);
 				break;
 			case 'p3':
-				pi(prio.p3);
+				pi(prio.p3, pColor.p3);
 				break;
 			case 'p4':
-				pi(prio.p4);
+				pi(prio.p4, pColor.p4);
 				break;
 			case 'comp':
-				pi(prio.comp);
+				pi(prio.comp, pColor.comp);
 		}
-		function pi(pi){
+		function pi(pi, color){
 			listItem.appendTo(pi);
+			let checkbox = pi.children().last().find('.check');
+			checkbox.css('border-color', color);
 			pi.children().last().slideDown('fast');
 			pi.children().last().css('display', 'flex');
 		}
